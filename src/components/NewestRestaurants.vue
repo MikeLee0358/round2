903e521771f710c5641d14d983ebc48fe524f081
MichaeLee1 <cspkno005@gmail.com>
2022-08-06 18:15:39 +0800

    done RestaurantsFeeds(SFC)

--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -4,38 +4,20 @@
       最新餐廳
     </div>
     <div class="card-body">
-      <div>
+
+      <div
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+      >
         <h4>
-          <a href="#">Deangelo Farrell</a>
-          <small>中式料理</small>
+          <a href="#">{{restaurant.name}}</a>
+          <small>{{ restaurant.Category ? restaurant.Category.name :'未分類'}}</small>
         </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
+        <p>{{restaurant.description}}</p>
+        {{restaurant.createdAt | fromNow}}
         <hr>
       </div>
-      <div>
-        <h4>
-          <a href="#">Carter Howe</a>
-          <small>日本料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr>
-      </div>
-      <div>
-        <h4>
-          <a href="#">Dr. Carole Smith</a>
-          <small>義大利料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr>
-      </div>
-      <div>
-        <h4>
-          <a href="#">Nyah Bernier</a>
-          <small>素食料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr>
-      </div>
+
     </div>
   </div>
 </template>
@@ -45,3 +27,23 @@ small {
   font-size: 10px;
 }
 </style>
+
+<script>
+import moment from 'moment'
+export default {
+  props: {
+    restaurants: {
+      type: Array,
+      required: true,
+    },
+  },
+  filters: {
+    fromNow(dateTime) {
+      if(!dateTime) return '-'
+
+      //使用moment提供的fromNow方法
+      return moment(dateTime).fromNow()
+    }
+  }
+};
+</script>
--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -9,12 +9,13 @@
     <div class="row">
       <div class="col-md-6">
         <h3>最新餐廳</h3>
-        <NewestRestaurants />
+        <NewestRestaurants :restaurants="restaurants" />
         <!-- 最新餐廳 NewestRestaurants -->
       </div>
       <div class="col-md-6">
         <!-- 最新評論 NewestComments-->
         <h3>最新評論</h3>
+        <NewestComments :comments="comments"/>
       </div>
     </div>
   </div>
@@ -24,6 +25,7 @@
 /* eslint-disable */
 import NavTabs from "../components/NavTabs.vue";
 import NewestRestaurants from "../components/NewestRestaurants.vue";
+import NewestComments from "../components/NewestComments.vue";
 const dummyData = {
   restaurants: [
     {
@@ -583,6 +585,7 @@ export default {
   components: {
     NavTabs,
     NewestRestaurants,
+    NewestComments,
   },
   methods: {
     fetchData() {
